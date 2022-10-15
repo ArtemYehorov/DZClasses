22245103e1fd78df8ce063997c352b4e304ed4d5
AzerX anDroID <artemyehorov@gmail.com>
2022-10-15 16:35:20 +0200

    ///

--- DZClasses/Group.cs
+++ DZClasses/Group.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +10,40 @@ using System.Xml.Linq;
 
 namespace DZClasses
 {
+
+    class MyEnumerator : IEnumerator
+    {
+        ArrayList ar; // поле, которое сможет выдать доступ к коллекции в любой части класса-перечислителя
+
+        // конструктор нужен для того, чтобы подружить коллекцию с перечислителем, чтобы перечислитель "увидел" что он собирается перебирать
+        public MyEnumerator(ArrayList ar)
+        {
+            this.ar = ar;
+        }
+
+        public object Current
+        {
+            get;
+            private set;
+        }
+
+        int index; // коллекцию (её инкапсулированный массив) будем перебирать начиная от индекса 0
+
+        public bool MoveNext()
+        {
+            if (index >= ar.Count)
+                return false;
+
+            Current = ar[index++]; // 5 15 47 28 37
+            return true;
+        }
+
+        public void Reset()
+        {
+            index = 0;
+        }
+    }
+
     /// <summary>
     /// Это группа для массива студентов.
     /// </summary>
@@ -18,6 +53,11 @@ namespace DZClasses
         private string NameOfCourse;
         private string Groupspecialization;
         private string NumberOfCourse;
+
+        public IEnumerator GetEnumerator()
+        {
+            return new MyEnumerator(group);
+        }
 
         public int CompareTo(Group some_another_group) // реализация интерфейса
         {
--- DZClasses/Program.cs
+++ DZClasses/Program.cs
@@ -15,36 +15,40 @@ namespace DZClasses
             g.Print();
             Console.WriteLine();
 
-            Group w = new Group(true,10);
-            w.Print();
-            Console.WriteLine();
-
-            Student q = new BadStudent();
-            Student a = new BadStudent();
-            Student e = new AverageStudent();
-            Student y = new GoodStudent();
-           
-            g.AddStudent(q);
-            g.AddStudent(y);
-            g.AddStudent(e);
-           
-            g.Print();
-            Console.WriteLine(e["Exams", 0]);
-            Console.WriteLine(e["TermPapers", 1]);
-            Console.WriteLine(e["OffSet", 2]);
-            g[5] = a;
-            Console.WriteLine();
-
-            if (y > e)
+            foreach (var item in g)
             {
-                Console.WriteLine("Студент y");
-                y.Print();
+                (item as Student).Print();
             }
-            else
-            {
-                Console.WriteLine("Студент е");
-                e.Print();
-            }
+            //Group w = new Group(true,10);
+            //w.Print();
+            //Console.WriteLine();
+
+            //Student q = new BadStudent();
+            //Student a = new BadStudent();
+            //Student e = new AverageStudent();
+            //Student y = new GoodStudent();
+           
+            //g.AddStudent(q);
+            //g.AddStudent(y);
+            //g.AddStudent(e);
+           
+            //g.Print();
+            //Console.WriteLine(e["Exams", 0]);
+            //Console.WriteLine(e["TermPapers", 1]);
+            //Console.WriteLine(e["OffSet", 2]);
+            //g[5] = a;
+            //Console.WriteLine();
+
+            //if (y > e)
+            //{
+            //    Console.WriteLine("Студент y");
+            //    y.Print();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Студент е");
+            //    e.Print();
+            //}
 
             Console.ReadLine();
         }
